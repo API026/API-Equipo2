621bfafeed07302d789707e8c6b5b83f8bde594c
ShiWonWan <emmanuelcampu@gmail.com>
2020-11-26 09:27:51 -0700

    db_config

--- db_config.py
+++ db_config.py
@@ -5,4 +5,4 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
 client = pymongo.MongoClient(f"mongodb+srv://user026:{DB_PASSWORD}@cluster0.ypown.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
-db = client.api_minecraft
+db = client.api_tv_shows
