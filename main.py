248e7a26eed6b0a88f856fca13f9e539bcf12de9
Hector Chavira <jon.chavira@hotmail.com>
2020-11-30 20:15:39 -0700

    creacion Endpoints

--- main.py
+++ main.py
@@ -15,6 +15,56 @@ def test():
         "message": "API working ok"
     })
 
+@app.route('/api_tv_shows/shows/', methods=['GET'])
+def show_mobs():
+    
+    shows = list(db.db.api_tv_shows.find())
+    for show in shows:
+        del show ["_id"]
+
+    return jsonify({"All_shows":shows})
+
+@app.route('/api_tv_shows/new_show/<string:tokenUser>', methods=['POST'])
+def add_new_show(tokenUser):
+    if tokenUser == secretToken:
+        db.db.api_tv_shows.insert_one({
+            "name": request.json["name"],
+            "category":request.json["category"],
+            "imdb":request.json["imdb"],
+            "top":request.json["top"],
+
+        })
+        return jsonify({
+            "message":" Success",
+            "status": 200,
+        })
+    else:
+        return jsonify({
+        "message":"Incorrect token",
+        "status": 700,
+    })
+
+@app.route('/api_tv_shows/update_show/<string:tokenUser>/<string:name>', methods=['PUT'])
+def update_show(tokenUser,name):
+    if tokenUser == secretToken:
+        if db.db.api_tv_shows.find_one({'name':name}):
+            db.db.api_tv_shows.update_one({'name':name},
+            {'$set':{
+                "name": request.json["name"],
+                "category":request.json["category"],
+                "imdb":request.json["imdb"],
+                "top":request.json["top"]
+            }})
+        else:
+            return jsonify({"status":400, "message": f"show {name} not found"})
+        return jsonify({"status":200, "message": f"show {name} was updated"})
+    else:
+        return jsonify({
+        "message":"Incorrect token",
+        "status": 700,
+    })
+
+
 
 if __name__ == '__main__':
     app.run(load_dotenv=True, port=8080)
